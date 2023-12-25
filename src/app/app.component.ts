3ff826b84d8314482a4eb8a1694bcd7322e850c5
Darsha Anuradha <122999771+darshaanuradha@users.noreply.github.com>
2023-12-25 18:55:16 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  items: any[] = [];
+  newItemName: string = '';
+  newItemDesc: string = '';
+
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit() {
+    this.itemService.getItems().subscribe(data => {
+      this.items = data.items;
+    });
+  }
+
+  addItem() {
+    const newItem = { name: this.newItemName, description: this.newItemDesc };
+    this.itemService.addItem(newItem).subscribe(response => {
+      console.log('Item added:', response);
+      this.items.push({ id: response.id, ...newItem });
+      this.newItemName = '';
+      this.newItemDesc = '';
+    });
+  }
 }
